79729396accd9f3a51793f99afef016490a72494
Amit Biswas <amit.142biswas@gmail.com>
2024-09-10 21:53:51 +0530

    auth-next sucks T.T

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -54,6 +54,12 @@ export const authOptions: NextAuthOptions = {
             return token
         },
         async session({ session, token }) {
+            if (token) {
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
             return session
         },
     },
